819bec593b4299c7238001a182a9cab1c29f6d1f
Vijaymishra26 <vijaymishra26@gmail.com>
2018-10-30 17:08:18 +0000

    Update HomeController.cs

--- SSTest.WebClient/Controllers/HomeController.cs
+++ SSTest.WebClient/Controllers/HomeController.cs
@@ -18,6 +18,8 @@ namespace SSTest.WebClient.Controllers
             ViewBag.Message = "Your application description page.";
             //Comments
             //coment by vijay
+            
+            //new changes for conflict
             return View();
         }
 
